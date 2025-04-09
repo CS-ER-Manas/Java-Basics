55454c93abc282e65401ff2cfe5801af97fed37b
Manas Kashyap <manaskashyap1372@gmail.com>
2025-04-09 10:49:22 +0530

    Split Function

--- src/main/java/org/example/SplitString.java
+++ src/main/java/org/example/SplitString.java
@@ -1,4 +1,12 @@
 package org.example;
 
 public class SplitString {
+    public static void main(String[] args) {
+        String str = "Hello,World,Java";
+        String[] parts = str.split(",");
+        for (String part : parts) {
+            System.out.println(part);
+        }
+    }
+
 }
